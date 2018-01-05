fb1db550da7cae480535bc35b80bc630348a02c1
Jimmy Sené <jimmy.sene@laposte.net>
2018-01-05 14:28:09 +0100

    Exo 2 structures : total commandes

--- Exo structures/main.c
+++ Exo structures/main.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct eleves
+/*struct eleves
 {
     char nom[20];
-    int age;
+    int age;;
     float salaire;
+};*/
+
+struct client
+{
+    char idc[20];
+    char nom[20];
+    int cp;
+};
+
+struct commande
+{
+    char idco[20];
+    char idc[20];
+    float tot;
 };
 
 int main()
 {
+    /*
+
+    ================= EXO 1 ==================
+
     int i;
     char nom[20];
     int trouve = 0;
@@ -35,6 +53,44 @@ int main()
            printf("Ce nom n'existe pas !");
         }
     }while(trouve == 0);
+
+     ================= EXO 2 ================== */
+
+     struct client cli[] = {"01", "dupont", 75000, "02", "jimmy", 93200};
+     struct commande com[] = {"01","01",80,"02", "01", 120, "03", "01", 26, "04", "02", 300, "05", "02", 67};
+
+     char nom[20];
+     int i, j, trouve = 0, total = 0;
+
+     do
+     {
+         printf("Entrez un nom : ");
+         gets(nom);
+
+         for(i = 0; i < 2; i++)
+         {
+             if(strcmp(nom, cli[i].nom)==0)
+             {
+                for(j = 0; j<5;j++)
+                {
+                    if(strcmp(cli[i].idc, com[j].idc) == 0)
+                    {
+                        total = total + com[j].tot;
+                    }
+                }
+                printf("Le cout total des commandes de %s est de %d euros !", nom, total);
+                trouve = 1;
+                break;
+             }
+         }
+
+         if(trouve == 0)
+         {
+             printf("Ce nom n'existe pas !");
+         }
+     }while(trouve == 0);
+
+
 
 
 
