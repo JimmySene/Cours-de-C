d257fd326ef56728445d13b7dba7917d854cb2dc
Jimmy Sené <jimmy.sene@laposte.net>
2018-01-05 13:52:25 +0100

    Exo structures

---
+++ Exo structures/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct eleves
+{
+    char nom[20];
+    int age;
+    float salaire;
+};
+
+int main()
+{
+    int i;
+    char nom[20];
+    int trouve = 0;
+
+    struct eleves sio1[] = {"dupont", 25, 3789.25, "leroy", 36, 1250.30};
+
+    printf("Entrez un nom : ");
+    gets(nom);
+    for(i = 0; i < 2; i++)
+    {
+        if(strcmp(nom, sio1[i].nom) == 0)
+        {
+            printf("Le salaire de %s est : %.2f €", nom, sio1[i].salaire);
+            trouve = 1;
+            break;
+        }
+    }
+
+    if(trouve == 0)
+    {
+       printf("Ce nom n'existe pas !");
+    }
+
+
+
+    return 0;
+}
