9095ebb83b5e5751f2b344860d8f06bea9f3a432
Jimmy Sené <jimmy.sene@laposte.net>
2018-01-05 14:09:59 +0100

    Finalisation première partie de l'exo structures

--- Exo structures/main.c
+++ Exo structures/main.c
@@ -16,23 +16,25 @@ int main()
     int trouve = 0;
 
     struct eleves sio1[] = {"dupont", 25, 3789.25, "leroy", 36, 1250.30};
-
-    printf("Entrez un nom : ");
-    gets(nom);
-    for(i = 0; i < 2; i++)
+    do
     {
-        if(strcmp(nom, sio1[i].nom) == 0)
+        printf("Entrez un nom : ");
+        gets(nom);
+        for(i = 0; i < 2; i++)
         {
-            printf("Le salaire de %s est : %.2f €", nom, sio1[i].salaire);
-            trouve = 1;
-            break;
+            if(strcmp(nom, sio1[i].nom) == 0)
+            {
+                printf("Le salaire de %s est : %.2f €", nom, sio1[i].salaire);
+                trouve = 1;
+                break;
+            }
         }
-    }
 
-    if(trouve == 0)
-    {
-       printf("Ce nom n'existe pas !");
-    }
+        if(trouve == 0)
+        {
+           printf("Ce nom n'existe pas !");
+        }
+    }while(trouve == 0);
 
 
 
